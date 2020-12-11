17390552a21eded9c2b7712f9d585870462bb824
violet <violet91410@gmail.com>
2020-12-11 23:01:40 +0900

    1st commit

---
+++ src/components/Company.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <!--header-->
+  <top />
+
+
+  <bottom/>
+
+
+</div>
+</template>
+
+<script>
+export default {
+name: "Introduction"
+}
+</script>
+
+<style scoped>
+
+</style>
